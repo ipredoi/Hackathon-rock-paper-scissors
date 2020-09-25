1643e2321be225d848bb2fa76bf84618ab04e9eb
Ionut <ipredoi27@gmail.com>
2020-09-25 12:31:25 +0100

    Task2FunctionCreated

--- main.js
+++ main.js
@@ -3,15 +3,9 @@
 let playerMove = ["rock", "paper", "scissors"];
 let computerMove = ["rock", "paper", "scissors"];
 
-if ((playerMove[0] === computerMove[0])||(playerMove[1] === computerMove[1])||(playerMove[2] === computerMove[2])) {
-    console.log("draw");
-}
-if ((playerMove[0] !== computerMove[2])||(playerMove[1] !== computerMove[0])||(playerMove[2] !== computerMove[1])) {
-    console.log("you win");
-}
-if ((playerMove[1] !== computerMove[2])||(playerMove[2] !== computerMove[0])||(playerMove[0] !== computerMove[1])) {
-    console.log("you lose");
-}
+
+
+
 
 // Task 2
 /*Take the if statements that you've written and put them into a function. The variables from before should now be taken in as parameters so that you can call the function with any two moves. Instead of printing the result to the console, the function should return...
@@ -32,6 +26,18 @@ let result = getWinner("rock", "paper");
 
 This will be deemed as complete when the function can be called with any combination of valid moves and returns the appropriate number. */
 
+function getWinner(playerMove, computerMove){
+    if ((playerMove[0] !== computerMove[2])||(playerMove[1] !== computerMove[0])||(playerMove[2] !== computerMove[1])) {
+        console.log(1);
+    }else{
+    if ((playerMove[0] === computerMove[0])||(playerMove[1] === computerMove[1])||(playerMove[2] === computerMove[2])) {
+        console.log(0);
+    }   
+    if ((playerMove[1] !== computerMove[2])||(playerMove[2] !== computerMove[0])||(playerMove[0] !== computerMove[1])) {
+        console.log(-1);
+    }}
+    return;
+}   
 
 
 
